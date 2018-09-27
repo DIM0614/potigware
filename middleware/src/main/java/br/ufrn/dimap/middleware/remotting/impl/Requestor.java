b4c9ef462472346c8ad366ab3dc10af5aa024034
Vitor Greati <greati@ufrn.edu.br>
2018-09-26 22:14:16 -0300

    Make Requestor implement RequestorI

--- middleware/src/main/java/br/ufrn/dimap/middleware/remotting/impl/Requestor.java
+++ middleware/src/main/java/br/ufrn/dimap/middleware/remotting/impl/Requestor.java
@@ -1,5 +1,12 @@
 package br.ufrn.dimap.middleware.remotting.impl;
 
-public class Requestor {
+import br.ufrn.dimap.middleware.remotting.interfaces.RequestorI;
+
+public class Requestor<T> implements RequestorI<T> {
+
+	public T invoke(String objectName, String operationName, Object... parameters) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
