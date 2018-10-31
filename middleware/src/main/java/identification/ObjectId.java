cadeada3e62d6dd3071472a9e8fa05f3c15df28b
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2018-10-31 19:36:19 -0300

    Add method to return least significant bits of ObjectId; Add method that overrides equals method

--- middleware/src/main/java/identification/ObjectId.java
+++ middleware/src/main/java/identification/ObjectId.java
@@ -29,4 +29,33 @@ public class ObjectId {
 	public ObjectId(String seed) {
 		this.objectId = UUID.fromString(seed);
 	}
+	
+	/**
+	 * Return the least significant 64 bits of this UUID's 128 bit value.
+	 * @return Least significant 64 bits.
+	 */
+	public long getObjectId() {
+		return this.objectId.getLeastSignificantBits();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+            return false;
+        }
+		
+		if (!ObjectId.class.isAssignableFrom(obj.getClass())) {
+			return false;
+		}
+		
+		final ObjectId param = (ObjectId) obj;
+		if(this.objectId.equals(param)) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 }
