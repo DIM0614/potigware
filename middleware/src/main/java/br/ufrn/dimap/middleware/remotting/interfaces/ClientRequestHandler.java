70837956fd70a6f6fff54c479958b2e42fb67e24
Victor <victor.agnez@hotmail.com>
2018-11-08 14:21:15 -0300

    Fix method signatures on client request handler

--- middleware/src/main/java/br/ufrn/dimap/middleware/remotting/interfaces/ClientRequestHandler.java
+++ middleware/src/main/java/br/ufrn/dimap/middleware/remotting/interfaces/ClientRequestHandler.java
@@ -19,65 +19,68 @@ public interface ClientRequestHandler {
 	 * Function used by the requestor to send the data,
 	 * using the specific protocol (synchronized)
 	 *  
-	 * @param host the hostname to send the data
+	 * @param host the host name to send the data
 	 * @param port the port to be used
 	 * @param msg the data to be sent
 	 * @return the server reply
+	 * @throws RemoteError if any error occurs
 	 */
-	ByteArrayInputStream send(String host, int port, ByteArrayOutputStream msg) throws RemoteError;
+	public ByteArrayInputStream send(String host, int port, ByteArrayOutputStream msg) throws RemoteError;
 	
 	/**
 	 * Function used by the requestor to send the data asynchronously
 	 * and then use the server's response as input of a callback method
 	 *  
-	 * @param host the hostname to send the data
+	 * @param host the host name to send the data
 	 * @param port the port to be used
 	 * @param msg the data to be sent
 	 * @param callback the callback object to be called after
 	 * @return the server reply
+	 * @throws RemoteError if any error occurs
 	 */
 
-	ByteArrayInputStream sendCallback(String host, int port, ByteArrayOutputStream msg, Callback callback) throws RemoteError;
+	public void sendWithCallback(String host, int port, ByteArrayOutputStream msg, Callback callback) throws RemoteError;
 	
 	/**
 	 * Function used by the requestor to send the data asynchronously
 	 * using the specified invocation asynchrony pattern
 	 *  
-	 * @param host the hostname to send the data
+	 * @param host the host name to send the data
 	 * @param port the port to be used
 	 * @param msg the data to be sent
 	 * @param callback the callback object to be called after
 	 * @return the server reply
+	 * @throws RemoteError if pattern is SyncWithServer or PollObject and any error occurs when trying to connect to server
 	 */
-	ByteArrayInputStream sendAsync(String host, int port, ByteArrayOutputStream msg, InvocationAsynchronyPattern pattern) throws RemoteError;
+	public void sendAsync(String host, int port, ByteArrayOutputStream msg, InvocationAsynchronyPattern pattern) throws RemoteError;
 
 	/**
 	 * @return the poll object
 	 */
-	PollObject getPollObject();
+	public PollObject getPollObject();
 
 	/**
 	 * @param pollObject the pollObject to set
 	 */
-	void setPollObject(PollObject pollObject);
+	public void setPollObject(PollObject pollObject);
 	
 	/**
 	 * @return the default protocol
 	 */
-	ClientProtocolPlugin getDefaultProtocol();
+	public ClientProtocolPlugin getDefaultProtocol();
 
 	/**
 	 * @param protocol the default protocol to set
 	 * @throws RemoteError if any error occurs when changing the Protocol
 	 */
-	void setDefaultProtocol(ClientProtocolPlugin protocol) throws RemoteError;
+	public void setDefaultProtocol(ClientProtocolPlugin protocol) throws RemoteError;
 
 	/**
 	 * Gets protocol to be used to communicate with the specific host
 	 * @param host the host
 	 * @return the protocol
 	 */
-	ClientProtocolPlugin getProtocol(String host);
+	public ClientProtocolPlugin getProtocol(String host);
 
 	/**
 	 * Sets the protocol to be used when communicating to a specific host 
@@ -86,7 +89,7 @@ public interface ClientRequestHandler {
 	 * @throws RemoteError if any error occurs when changing the protocol
 	 * @throws UnknownHostException if it didn't find the specified host
 	 */
-	void setProtocol(String host, ClientProtocolPlugin protocol) throws RemoteError, UnknownHostException;
+	public void setProtocol(String host, ClientProtocolPlugin protocol) throws RemoteError, UnknownHostException;
 
 	/**
 	 * Gets protocol to be used to communicate with the specified host
@@ -94,7 +97,7 @@ public interface ClientRequestHandler {
 	 * @param host the host
 	 * @return the protocol
 	 */
-	ClientProtocolPlugin getProtocol(String host, int port);
+	public ClientProtocolPlugin getProtocol(String host, int port);
 
 	/**
 	 * Sets the protocol to be used when communicating to a
@@ -104,6 +107,11 @@ public interface ClientRequestHandler {
 	 * @throws RemoteError if any error occurs when changing the protocol
 	 * @throws UnknownHostException if it didn't find the specified host
 	 */
-	void setProtocol(String host, int port, ClientProtocolPlugin protocol) throws RemoteError, UnknownHostException;
+	public void setProtocol(String host, int port, ClientProtocolPlugin protocol) throws RemoteError, UnknownHostException;
+	
+	/**
+	 * Shutdowns all the plug-ins
+	 */
+	public void shutdown();
 
 }
