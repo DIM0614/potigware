73590099d944fdcaea69c5163f546f32cd4151e8
Irene <ireneginani@gmail.com>
2018-11-11 20:13:21 -0300

    made changes to the lookup interface

--- middleware/src/main/java/br/ufrn/dimap/middleware/identification/lookup/Lookup.java
+++ middleware/src/main/java/br/ufrn/dimap/middleware/identification/lookup/Lookup.java
@@ -1,16 +1,13 @@
 package br.ufrn.dimap.middleware.identification.lookup;
 
 import br.ufrn.dimap.middleware.identification.AbsoluteObjectReference;
+import br.ufrn.dimap.middleware.identification.ObjectId;
 import br.ufrn.dimap.middleware.remotting.impl.RemoteError;
 
 /**
  * Implements a Lookup Service as part of the distributed object middleware. 
  * This service provides an way to bind some name to an remote object absolute 
  * reference, and other way to find this absolute reference given an name.
- * 
- * @author Yuri Alessandro Martins 
- * @version 1.0
- * @see AbsoluteObjectReference
  * @see RemoteError
  */
 public interface Lookup {
@@ -20,10 +17,12 @@ public interface Lookup {
 	 * 
 	 * @param name	Propertied that'll serve as identifier to absolute object 
 	 * 				reference.
-	 * @param aor	The absolute object reference that must be binded with the 
+	 * @param remoteObject	The absolute object reference that must be binded with the 
 	 * 				given name (propertied).
+	 * @param host	host where is the resource
+	 * @param port	port that localizate the resource
 	 */
-	public void bind(String name, AbsoluteObjectReference aor) throws RemoteError;
+	public void bind(String name, Object remoteObject, String host, String port) throws RemoteError;
 	
 	/**
 	 * Allow clients to use lookup service to query for the ABSOLUTE OBJECT 
@@ -33,5 +32,15 @@ public interface Lookup {
 	 * 				previously binded with this same name.
 	 * @return		The absolute object reference binded with the given name.
 	 */
-	public AbsoluteObjectReference find(String name) throws RemoteError;;
+	public AbsoluteObjectReference find(String name) throws RemoteError;
+	
+	/**
+	 * Allow clients to use lookup service to query for the ABSOLUTE OBJECT 
+	 * REFERENCES of remote objects based the name.
+	 * 
+	 * @param ObjectId	ObjectId that'll be used to find the absolute object reference, 
+	 * 				previously binded with this same ObjectId.
+	 * @return		The absolute object reference binded with the given the ObjectId.
+	 */
+	public AbsoluteObjectReference findById(ObjectId ObjectId) throws RemoteError;;
 }
