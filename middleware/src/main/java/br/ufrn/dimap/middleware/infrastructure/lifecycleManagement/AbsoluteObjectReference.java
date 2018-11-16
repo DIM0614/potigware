c026a4545a18cee84572b5f6d2aabd22d23ffc31
Diogo <Diogo@10.51.64.247>
2018-11-16 11:12:55 -0300

    Remove the class example AOR.

--- middleware/src/main/java/br/ufrn/dimap/middleware/infrastructure/lifecycleManagement/AbsoluteObjectReference.java
+++
@@ -1,9 +0,0 @@
-package br.ufrn.dimap.middleware.infrastructure.lifecycleManagement;
-
-public class AbsoluteObjectReference {
-
-	public AbsoluteObjectReference() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
