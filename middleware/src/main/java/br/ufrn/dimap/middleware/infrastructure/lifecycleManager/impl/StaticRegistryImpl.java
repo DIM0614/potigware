df34e04dfcb042bf2fd8448ed6ef54d917ab06b1
Diogo Souza <di0g0-dg@Hotmail.com>
2018-11-16 14:17:22 -0200

    Added necessary imports.

--- middleware/src/main/java/br/ufrn/dimap/middleware/infrastructure/lifecycleManager/impl/StaticRegistryImpl.java
+++ middleware/src/main/java/br/ufrn/dimap/middleware/infrastructure/lifecycleManager/impl/StaticRegistryImpl.java
@@ -1,6 +1,10 @@
 package br.ufrn.dimap.middleware.infrastructure.lifecycleManager.impl;
 
+import java.util.HashMap;
+
+import br.ufrn.dimap.middleware.identification.ObjectId;
 import br.ufrn.dimap.middleware.infrastructure.lifecycleManager.interfaces.StaticRegistry;
+import br.ufrn.dimap.middleware.remotting.interfaces.Invoker;
 
 public class StaticRegistryImpl implements StaticRegistry
 {
@@ -21,12 +25,11 @@ public class StaticRegistryImpl implements StaticRegistry
 	private Invoker createObj(ObjectId id)
 	{
 		// Verify type of object for instantiate.
-		Invoker obj = new ObjectType();
+		Invoker obj = null; // = new ObjectType();
 		
 		staticRegistry.put(id, obj);
 		
 		
 		return obj;
 	}
-
 }
