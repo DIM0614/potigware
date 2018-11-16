a6412ef5d960da1e19fbcbbfeb0bca04b430ca56
Diogo Souza <di0g0-dg@Hotmail.com>
2018-11-16 14:15:10 -0200

    Added necessary import from Invoker and ObjectId.

--- middleware/src/main/java/br/ufrn/dimap/middleware/infrastructure/lifecycleManager/interfaces/StaticRegistry.java
+++ middleware/src/main/java/br/ufrn/dimap/middleware/infrastructure/lifecycleManager/interfaces/StaticRegistry.java
@@ -1,5 +1,8 @@
 package br.ufrn.dimap.middleware.infrastructure.lifecycleManager.interfaces;
 
+import br.ufrn.dimap.middleware.identification.ObjectId;
+import br.ufrn.dimap.middleware.remotting.interfaces.Invoker;
+
 public interface StaticRegistry
 {
 	public Invoker getObj(ObjectId id);
