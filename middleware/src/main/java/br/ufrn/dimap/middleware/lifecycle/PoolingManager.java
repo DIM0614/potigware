6b3957d20f4c0c32fcf057be6cd7421791969c23
Wilson Farias <wilsonsf1@gmail.com>
2018-11-20 10:32:49 -0300

    Changes in PoolingI interface and Manager impl.
    Addes generics to interface PoolingI
    Delegates private method to Manager impl.

--- middleware/src/main/java/br/ufrn/dimap/middleware/lifecycle/PoolingManager.java
+++ middleware/src/main/java/br/ufrn/dimap/middleware/lifecycle/PoolingManager.java
@@ -1,40 +1,81 @@
 package br.ufrn.dimap.middleware.lifecycle;
 
+import jdk.nashorn.internal.ir.RuntimeNode;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class PoolingManager<T>{
-	
-	private String type;
-	private int size;
-	private List<Integer> pool;
-	
-	public PoolingManager(String type, int size) {
-		this.type = type;
-		this.size = size;
-		pool = new ArrayList<Integer>(size);
-		for (int i=0; i<size; i++) {
-			pool.add(i);
-		}
-	}
-	
-	public int removeFromPool(int pooledSrvt) throws InterruptedException {
-		synchronized(pool) {
-			while(pool.size() <= 0) {
-				//pool.wait();
-			}
-			return pool.remove(pooledSrvt);
-		}	
-	}
-	
-	public void putBackToPool(int pooledSrvt) {
-		synchronized(pool) {
-			pool.add(pooledSrvt);
-			//pool.notify();
-			System.out.println("Tamanho: " + pool.size());
-		}
-	}
-	
-	
+public class PoolingManager<T extends PerRequest> implements PoolingI<T> {
 
+    private String type;
+    private int size;
+    private List<T> pool;
+
+    public PoolingManager(String type, int size) {
+        this.type = type;
+        this.size = size;
+        pool = new ArrayList<>(size);
+
+        Class<?> aClass;
+        try {
+            aClass = Class.forName(type);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Classe não existe - jumento!", e);
+        }
+
+        try {
+            for (int i = 0; i < size; i++) {
+                Object instance = aClass.newInstance();
+                addPoolInstance((T) instance);
+            }
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e.getLocalizedMessage());
+        }
+    }
+
+    /**
+     * This method should add an object to the pool.
+     *
+     * @param pooledServant will be the servant to be placed in the pool.
+     */
+    private boolean addPoolInstance(T pooledServant) {
+        return pool.add(pooledServant);
+    }
+
+    public boolean removeFromPool() throws InterruptedException {
+//        synchronized (pool) {
+//            while (pool.size() <= 0) {
+//                //pool.wait();
+//            }
+//            return pool.remove(pooledSrvt);
+//        }
+        return false;
+    }
+
+    public void putBackToPool(int pooledSrvt) {
+//        synchronized (pool) {
+//            pool.add(pooledSrvt);
+//            //pool.notify();
+//            System.out.println("Tamanho: " + pool.size());
+//        }
+    }
+
+    @Override
+    public T getFreeInstance() {
+        return null;
+    }
+
+    /**
+     * This method will retrieve a servant from the pool to be used by the
+     * client.
+     *
+     * @param pooledServant servant to be removed and used.
+     */
+//    private boolean removeFromPool(T pooledServant) {
+//        return false;
+//    }
+    @Override
+    public boolean putBackToPool(Servant pooledServant) {
+        return false;
+    }
 }
