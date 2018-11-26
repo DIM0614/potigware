b81c54345494066126a9cb9eb43b4cc3898ad491
Victor <victor.agnez@hotmail.com>
2018-11-26 15:22:11 -0300

    Add UUID attribute

--- middleware/src/main/java/br/ufrn/dimap/middleware/remotting/impl/Invocation.java
+++ middleware/src/main/java/br/ufrn/dimap/middleware/remotting/impl/Invocation.java
@@ -1,6 +1,7 @@
 package br.ufrn.dimap.middleware.remotting.impl;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -15,10 +16,15 @@ import br.ufrn.dimap.middleware.extension.impl.InvocationContext;
 @XmlRootElement(name = "Invocation")
 public class Invocation implements Serializable {
 
-    private InvocationData invocationData;
+	private static final long serialVersionUID = -1602846527685641748L;
+	
+	private final UUID invocationId;
+	
+	private InvocationData invocationData;
 
     public Invocation() {
-		super();
+    	super();
+    	this.invocationId = UUID.randomUUID();
 	}
 
 	private InvocationContext context;
@@ -26,11 +32,13 @@ public class Invocation implements Serializable {
     public Invocation(InvocationData invocationData) {
         this.invocationData = invocationData;
         this.context = new InvocationContext();
+        this.invocationId = UUID.randomUUID();
     }
 
     public Invocation(InvocationData invocationData, InvocationContext context) {
         this.invocationData = invocationData;
         this.context = context;
+        this.invocationId = UUID.randomUUID();
     }
 
     public InvocationData getInvocationData() {
@@ -55,6 +63,7 @@ public class Invocation implements Serializable {
 		int result = 1;
 		result = prime * result + ((context == null) ? 0 : context.hashCode());
 		result = prime * result + ((invocationData == null) ? 0 : invocationData.hashCode());
+		result = prime * result + ((invocationId == null) ? 0 : invocationId.hashCode()); 
 		return result;
 	}
 
@@ -67,6 +76,11 @@ public class Invocation implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Invocation other = (Invocation) obj;
+		if (invocationId == null) {
+			if (other.invocationId != null)
+				return false;
+		} else if (!invocationId.equals(other.invocationId))
+			return false;
 		if (context == null) {
 			if (other.context != null)
 				return false;
