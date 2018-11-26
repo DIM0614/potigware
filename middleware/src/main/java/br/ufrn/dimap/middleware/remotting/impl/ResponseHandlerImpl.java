7bee5971e23a0e9f90787675eedb1484aab9552e
Victor <victor.agnez@hotmail.com>
2018-11-26 16:08:58 -0300

    Fix bug when calling qos

--- middleware/src/main/java/br/ufrn/dimap/middleware/remotting/impl/ResponseHandlerImpl.java
+++ middleware/src/main/java/br/ufrn/dimap/middleware/remotting/impl/ResponseHandlerImpl.java
@@ -1,6 +1,9 @@
 package br.ufrn.dimap.middleware.remotting.impl;
 
 import java.io.ByteArrayInputStream;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
 
 import br.ufrn.dimap.middleware.identification.AbsoluteObjectReference;
 import br.ufrn.dimap.middleware.infrastructure.lifecycleManager.impl.LifecycleManagerImpl;
@@ -19,7 +22,7 @@ import br.ufrn.dimap.middleware.extension.interfaces.ResponseHandler;
  */
 public class ResponseHandlerImpl implements ResponseHandler {
 	
-	private final Marshaller marshaller = new JavaMarshaller(); 
+	private final Marshaller marshaller = new XMLMarshaller(); 
 	private final LifecycleManager lifecycleManager;
 	private final QoSObserver qosObserver;
 	
@@ -37,15 +40,26 @@ public class ResponseHandlerImpl implements ResponseHandler {
 		InvocationData invocationData;
 		AbsoluteObjectReference aor;
 		Invoker invoker;
+		@SuppressWarnings("rawtypes")
+		Set<Class > params = new HashSet<Class>();
 		
 		try {
 			invocation = marshaller.unmarshal(new ByteArrayInputStream(msg), Invocation.class);
 			invocationData = invocation.getInvocationData();
 			aor = invocationData.getAor();
 
-			qosObserver.started(invocation, msg.length);
-
 			invoker = lifecycleManager.getInvoker(aor);
+			
+			for(Method m : invoker.getClass().getMethods()) {
+				for(Class<?> c : m.getParameterTypes()) {
+					params.add(c);
+				}
+			}
+			
+			invocation = marshaller.unmarshal(new ByteArrayInputStream(msg), Invocation.class, params);
+
+			qosObserver.started(invocation, msg.length);
+			
 		} catch(Exception e) {
 			throw new RemoteError(e);
 		}
