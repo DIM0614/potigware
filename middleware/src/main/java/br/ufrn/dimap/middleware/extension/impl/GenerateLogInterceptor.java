d5a01fa700105cbe1470eac0fd8a9bf67ad55f6f
giovannirosario <giovanni.rosario@hotmail.com>
2018-11-25 23:28:33 -0300

    Add class GenerateLogInterceptor

---
+++ middleware/src/main/java/br/ufrn/dimap/middleware/extension/impl/GenerateLogInterceptor.java
@@ -0,0 +1,63 @@
+package br.ufrn.dimap.middleware.extension.impl;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import br.ufrn.dimap.middleware.extension.interfaces.InvocationInterceptorUnserialized;
+import br.ufrn.dimap.middleware.identification.AbsoluteObjectReference;
+import br.ufrn.dimap.middleware.remotting.impl.InvocationData;
+import br.ufrn.dimap.middleware.remotting.impl.RemoteError;
+
+/**  
+ * Example of a Interceptor to generate Invocation Logs
+ * 
+ * @author giovannirosario
+ */
+
+public class GenerateLogInterceptor implements InvocationInterceptorUnserialized {
+	private Logger logger;
+	FileHandler fh;
+	
+	public GenerateLogInterceptor (String path_file, Boolean append) {
+		try {
+			fh = new FileHandler(path_file, append);
+			logger.addHandler(fh);
+			SimpleFormatter formatter = new SimpleFormatter();  
+	        fh.setFormatter(formatter);  
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void beforeInvocation(InvocationData invocationData, InvocationContext invocationContext) throws RemoteError {
+		String msg;
+		String operationName = invocationData.getOperationName();
+		AbsoluteObjectReference aor = invocationData.getAor();
+		String aor_id = Long.toString(aor.getObjectId().getObjectId());
+		String aor_host = aor.getHost();
+		String aor_port = Integer.toString(aor.getPort());
+		
+		msg = "Preparing to invoke operation " + aor_id + " " + operationName + " at " + aor_host + ":" + aor_port;
+
+		logger.info(msg);
+	}
+
+	public void afterInvocation(InvocationData invocationData, InvocationContext invocationContext) throws RemoteError {
+		String msg;
+		String operationName = invocationData.getOperationName();
+		AbsoluteObjectReference aor = invocationData.getAor();
+		String aor_id = Long.toString(aor.getObjectId().getObjectId());
+		String aor_host = aor.getHost();
+		String aor_port = Integer.toString(aor.getPort());
+		
+		msg = "Finished invocation of operation " + aor_id + " " + operationName + " at " + aor_host + ":" + aor_port;
+
+		logger.info(msg);
+	}
+}
