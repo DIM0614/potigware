38c7489e49e111b2354d3d48a7c15a9409b4083a
Carlos Vieira <vieiramecarlos@gmail.com>
2018-11-27 01:38:11 -0300

    Update exception handling for marshalling

--- middleware/src/main/java/br/ufrn/dimap/middleware/remotting/impl/DefaultClientProtocol.java
+++ middleware/src/main/java/br/ufrn/dimap/middleware/remotting/impl/DefaultClientProtocol.java
@@ -281,7 +281,7 @@ public class DefaultClientProtocol implements ClientProtocolPlugIn {
 			if(returnValue instanceof VoidObject)
 				returnValue = null;
 			callback.onResult(returnValue);
-		} catch (ClassNotFoundException | IOException e) {
+		} catch (MarshallerException e) {
 			callback.onError(new RemoteError(e));
 		} catch (RemoteError e) {
 			callback.onError(e);
@@ -320,7 +320,7 @@ public class DefaultClientProtocol implements ClientProtocolPlugIn {
 				if(returnValue instanceof VoidObject)
 					returnValue = null;
 				pollObject.storeResult(returnValue);
-			} catch(IOException | ClassNotFoundException e) {
+			} catch(MarshallerException e) {
 				throw new RemoteError(e);
 			}
 		} catch (RemoteError e) {
